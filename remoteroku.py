801a3ec0afbd86186bc6f79e24a9b43d7cce4f45
Samuel Lord <sllord@icloud.com>
2018-08-13 14:59:00 -0500

    Create remoteroku.py

---
+++ remoteroku.py
@@ -0,0 +1,26 @@
+from roku import Roku
+rokuls = Roku.discover(timeout=10);
+print(rokuls);
+ip =  input('IP: ');
+roku = Roku(ip);
+
+while True:
+	remoteinput = input('Remote Input (i.e. down, up, left, right, back, home, ok): ');
+
+	if remoteinput == 'down':
+		roku.down();
+	elif remoteinput == 'up':
+		roku.up();
+	elif remoteinput == 'left':
+		roku.left();
+	elif remoteinput == 'right':
+		roku.right();
+	elif remoteinput == 'back':
+		roku.back();
+	elif remoteinput == 'home':
+		roku.home();
+	elif remoteinput == 'ok':
+		roku.select();
+	else:
+		print('Not a valid command.');
+
